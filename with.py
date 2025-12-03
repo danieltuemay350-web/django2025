12b88d706e1a02de41d56dd2889f6a9807500f42
daniel <danieltuemay350@gmail.com>
2025-12-03 22:05:40 +0300

    with practice

--- with.py
+++ with.py
@@ -1,7 +1,36 @@
-with open("calcultor/config.txt", "w") as file:
-    file.write('daniel is a good boy')
-with open("calcultor/config.txt", "r") as file:
-    content = file.read()
-    print(content)
-with open("calcultor/config.txt", "") as file:
-    file.write('\nwelcome to the calcultor program')
+"""
+
+
+    with open("calcultor/config.txt", "w") as file:
+        file.write('daniel is a good boy')
+    with open("calcultor/config.txt", "r") as file:
+        content = file.read()
+        print(content)
+
+
+"""
+
+
+
+
+
+path_config=r'calcultor/config.txt'
+
+try:
+    with open(path_config, "w") as file:
+        file.write('guest')
+except FileNotFoundError as e:
+    with open(path_config, "w") as file:  
+        file.write('the file was created because it was not found')
+    print('the file was not found', file)
+
+
+else:
+    with open(path_config, "r") as file:
+        content = file.read()
+        print(content)
+    print('the file was written successfully')
+
+
+finally:
+    print('the program has ended')
